dc841816bd4b600ea6ec2d5d040b35db3aaa5bc5
Arab_Theeb <theeb1230@gmail.com>
2026-09-19 03:23:11 +0300

    fix: restore egydead HTTP module import

--- src/providers/egydead/index.ts
+++ src/providers/egydead/index.ts
@@ -1,7 +1,7 @@
 import { BaseProvider } from '../base.js';
 import { ProviderDetail, ProviderEpisode, ProviderItem, ResolvedStream } from '../../types/provider.js';
 import { StremioContentType } from '../../types/stremio.js';
-import { HttpClient, MOBILE_USER_AGENT } from '../../utils/this.http.js';
+import { HttpClient, MOBILE_USER_AGENT } from '../../utils/http.js';
 import { extractStreams } from '../../extractors/index.js';
 
 export class EgydeadProvider extends BaseProvider {
