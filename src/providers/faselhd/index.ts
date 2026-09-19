48d9611a6326b98994b35636df3c6f9333dfaa8e
Arab_Theeb <theeb1230@gmail.com>
2026-09-19 03:23:14 +0300

    fix: restore faselhd HTTP module import

--- src/providers/faselhd/index.ts
+++ src/providers/faselhd/index.ts
@@ -2,7 +2,7 @@ import vm from 'node:vm';
 import { BaseProvider } from '../base.js';
 import { ProviderDetail, ProviderEpisode, ProviderItem, ResolvedStream } from '../../types/provider.js';
 import { StremioContentType } from '../../types/stremio.js';
-import { HttpClient, MOBILE_USER_AGENT } from '../../utils/this.http.js';
+import { HttpClient, MOBILE_USER_AGENT } from '../../utils/http.js';
 import { extractStreams } from '../../extractors/index.js';
 
 export class FaselhdProvider extends BaseProvider {
