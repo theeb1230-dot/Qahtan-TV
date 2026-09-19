9b5be646d9133e492d2b17a64e8096539043f0ab
Arab_Theeb <theeb1230@gmail.com>
2026-09-19 03:23:03 +0300

    fix: restore akwam HTTP module import

--- src/providers/akwam/index.ts
+++ src/providers/akwam/index.ts
@@ -1,7 +1,7 @@
 import { BaseProvider } from '../base.js';
 import { ProviderDetail, ProviderEpisode, ProviderItem, ResolvedStream } from '../../types/provider.js';
 import { StremioContentType } from '../../types/stremio.js';
-import { HttpClient } from '../../utils/this.http.js';
+import { HttpClient } from '../../utils/http.js';
 import { extractStreams } from '../../extractors/index.js';
 
 export class AkwamProvider extends BaseProvider {
